cdd7c3100fb07a26de4d1620305887371a0e6743
Mostak <mostakahmedshahad74@gmail.com>
2025-02-20 03:44:09 +0600

    Increment_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
- 
+import 'package:app_10/bloc/counter/counter_bloc.dart';
 import 'package:app_10/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
- 
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,10 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:CounterScreen(),
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: CounterScreen(),
+      ),
     );
   }
 }
- 
--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -1,4 +1,8 @@
+import 'package:app_10/bloc/counter/counter_bloc.dart';
+import 'package:app_10/bloc/counter/counter_event.dart';
+import 'package:app_10/bloc/counter/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -8,23 +12,49 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  
+
   @override
   Widget build(BuildContext context) {
+     
     return Scaffold(
-
-      appBar: AppBar(title: Text("Counter App"),),
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
-          Center(child: Text('0',style: TextStyle(fontSize: 10),)),
+
+          BlocBuilder<CounterBloc, CounterState>(
+            builder: (context, state) {
+              return  Center(
+              child: Text(
+          state.counter.toString(),
+            style: TextStyle(fontSize: 50),
+          )) ;
+            },
+          ),
+         
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        
-
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () { 
+                    context.read<CounterBloc>().add(IncrementCounter());
+                    
+                  },
+                  child: Text('Increment')),
+              SizedBox(
+                width: 15,
+              ),
+              ElevatedButton(
+                  onPressed: () { 
+                    
+                  },
+                  child: Text('Dcrement')),
+            ],
           )
         ],
       ),
